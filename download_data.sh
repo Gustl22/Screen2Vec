a2fa6a8f2a45207a2bb9824d3525cc86c51329d5
Gustl22 <git@reb0.org>
2023-05-09 13:19:13 +0200

    feat: delete downloaded zip files to save space

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,8 @@
 
 wget https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/traces.tar.gz
 tar -xf traces.tar.gz
+rm traces.tar.gz
 
 wget https://storage.googleapis.com/crowdstf-rico-uiuc-4540/rico_dataset_v0.1/unique_uis.tar.gz
 tar -xf unique_uis.tar.gz
+rm unique_uis.tar.gz
